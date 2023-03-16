4c2e0d1a733e0b2e8ceb1edd45d8a1409abde51f
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-03-16 12:10:36 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,15 +3,20 @@ import boto3
 
 def get_regions(regionname):
     ec2 = boto3.client('ec2',
-                       aws_access_key_id='xxxxxxxxxxxx4',
-                       aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+                       aws_access_key_id='xxxxxxxxxxxxxxxxxx',
+                       aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
                        region_name=regionname
 
                        )
-    vm = ec2.describe_instances().get('Reservations','thsi is not there')
-    print(vm)
-
-
-get_regions('ap-northeast-1')
-print('=============================================================')
-get_regions('us-east-1')
+    vms = ec2.describe_instances().get('Reservations', 'thsi is not there')
+    my_vms = []
+    for vm in vms:
+        for v in vm['Instances']:
+            # print(v['InstanceType'])
+            my_vms.append(v['InstanceId'])
+    
+    
+    return my_vms
+a =get_regions('ap-northeast-1')
+print(a)
+# print('=============================================================')
